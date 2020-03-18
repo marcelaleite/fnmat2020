54446ff29c2dcac34839c28c711313427079eb03
Mumuzinho12 <52547341+Mumuzinho12@users.noreply.github.com>
2020-03-18 12:52:29 -0300

    nav

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,18 @@
-window.onscroll = scroll;
-
-function scroll() {
-var scrollTop = window.pageYOffset;
-if (scrollTop > 30) {
-try {
-document.getElementById('antes').id = 'depois'
-} catch (e) {
-false
-}
-} else {
-try {
-document.getElementById('depois').id = 'antes'
-} catch (e) {
-false
-}
-}
-};
+// window.onscroll = scroll;
+//
+// function scroll() {
+// var scrollTop = window.pageYOffset;
+// if (scrollTop > 30) {
+// try {
+// document.getElementById('antes').id = 'depois'
+// } catch (e) {
+// false
+// }
+// } else {
+// try {
+// document.getElementById('depois').id = 'antes'
+// } catch (e) {
+// false
+// }
+// }
+// };
