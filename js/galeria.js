d1864923f1545b7e8d5d83392960bf5f2f0b4f9e
Mumuzinho12 <52547341+Mumuzinho12@users.noreply.github.com>
2020-03-18 15:36:06 -0300

    galeria

--- js/galeria.js
+++ js/galeria.js
@@ -1,11 +1,10 @@
-var instance = M.Carousel.init({
-fullWidth: true,
-indicators: true
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems);
 });
 
 // Or with jQuery
 
-$('.carousel.carousel-slider').carousel({
-fullWidth: true,
-indicators: true
+$(document).ready(function(){
+  $('.carousel').carousel();
 });
--- js/slide.js
+++ js/slide.js
@@ -1,16 +1,1 @@
 
-$(document).ready(function(){
-    $('.slider').slider();
-  });
-
-var instance = M.Carousel.init({
-fullWidth: true,
-indicators: true
- });
-
-// Or with jQuery
-
-$('.carousel.carousel-slider').carousel({
-fullWidth: true,
-indicators: true
-});
